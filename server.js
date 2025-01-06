1108fda1fe86cd6e7e0b687de7dae1d5111c8561
Er-som03 <163736513+Er-som03@users.noreply.github.com>
2025-01-06 20:21:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ http.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
 })
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname))
 
 
 app.get('/',(req,res) =>{
